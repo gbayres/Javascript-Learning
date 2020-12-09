6276c830c6fe5af7bdee27a7216ab542321cf095
Gabriel Ayres <gbayres@hotmail.com>
2020-12-09 14:28:18 -0300

    Studies 09/12/20

--- Courses/hCode/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ Courses/hCode/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -1,7 +1,8 @@
 class CalcController {
     
     constructor(){
-       
+        
+        this._operation = [""];
         this._locale = "pt-BR";
         this._$displayCalcEl = document.querySelector('#display');
         this._$dateEl = document.querySelector('#data');
@@ -22,16 +23,99 @@ class CalcController {
     
     addEventListenerAll(element, events, fn){
         events.split(" ").forEach(event => {
-            element.addEventListener(event, fn);
+            element.addEventListener(event, fn, false);
         })
     }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop()
+
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+    }
+
+    getLastOperation(){
+        return this._operation[_operation.length - 1];
+    }
+
+    execBtn(value){
+
+        let lastItem = this._operation[length.]
+
+        if (isNaN(value)){
+            this.addOperation(value);
+            this.addOperation("");
+        } else{
+            this._operation.push(String(this._operation.pop()) + String(value));    
+        }
+
+        console.log(this._operation);
+        
+        // switch(value){
+        //     case 'ac':
+        //         this.clearAll();
+        //     break;
+            
+        //     case 'ce':
+        //         this.clearEntry();
+        //     break;
+            
+        //     case 'soma':
+
+        //     break;
+
+            
+        //     case 'subtracao':
+
+        //     break;
+
+            
+        //     case 'divisao':
+
+        //     break;
+
+            
+        //     case 'multiplicacao':
+
+        //     break;
+
+        //     case 'porcento':
+
+        //     break;
+
+        //     case 'igual':
+
+        //     break;
+
+        //     case 
+
+        //     default:
+        //         this.setError();
+        //         break;
+
+        // }
+    }
+
+    setError(){
+        this.displayCalc = "Error";
+    }
+
 
     initButtonsEvents(){
         let $buttons = document.querySelectorAll("#buttons > g, #parts > g");
         
         $buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, "click drag", (e) => {
-                console.log(btn.className.baseVal.replace("btn-",""));
+
+                let textBtn = btn.className.baseVal.replace("btn-","");
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
