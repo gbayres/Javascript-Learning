e3faf55f8fafd0b208b16ec43837c6cdd75e79db
Gabriel Ayres <gbayres@hotmail.com>
2021-03-28 13:46:47 -0300

    Studies 28/03/21

--- React/learningreact/src/App.js
+++ React/learningreact/src/App.js
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Comentario from './components/comentario';
+import Comentario, {BUTTON, LOADING, FORMULARIO} from './components/comentario';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
-      <Comentario />
-    </div>
-  );
+class App extends Component{
+  state = {
+    comentarios: [
+      {
+        name: "João",
+        email: "joao@gmail.com",
+        data: new Date(2020, 3, 19),
+        message: "Olá, tudo bem?"
+      },
+      {
+        name: "Pedro",
+        email: "pedrim@gmail.com",
+        data: new Date(2020, 3, 22),
+        message: "Tudo bem sim, meu caro"
+      }
+    ]
+  }
+  
+  adicionarComentario = () =>{
+    console.log("Adicionando comentário");
+
+    const NOVO_COMENTARIO = {
+      name: "Maria",
+      email: "maria@gmail.com",
+      data: new Date(),
+      message: "Olá pessoal!!!!"
+    }
+
+    this.setState({
+      comentarios: [...this.state.comentarios, NOVO_COMENTARIO]
+    })
+  }
+
+  removerComentario = () =>{
+    this.setState({
+      comentarios: [...this.state.comentarios].slice(0,-1),
+    })
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <h1>Meu projeto</h1>
+        {this.state.comentarios.map(comentario => {
+          return(
+          <Comentario 
+          name={comentario.name}
+          email={comentario.email}
+          data={comentario.data}>
+            {comentario.message}
+          </Comentario>
+          )
+        })}
+
+        <BUTTON onClick={this.adicionarComentario}>Adicionar um comentário</BUTTON>
+        <BUTTON onClick={this.removerComentario}>Remover um comentário</BUTTON>
+        <FORMULARIO></FORMULARIO>
+        
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- React/learningreact/src/components/comentario.js
+++ React/learningreact/src/components/comentario.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import './comentario.css';
 
-const Comentario = () => (
-    <div>Olá, eu sou seu pai.</div>
-);
+const Comentario = props => {
+    return (
+        <div className="Comentario">
+            <h2>{props.name}</h2>
+            <p>{props.email}</p>
+            <p>{props.children}</p>
+            <p>{props.data.toString()}</p>
+        </div>
+)}
+
+const BUTTON = props => {
+    return (
+        <button onClick={props.onClick} className="blueBtn redBtn">{props.children}</button>
+    )
+}
+
+const LOADING = props => {
+    return (
+        <div className="divToda">
+            <div className="ball b1"></div>
+            <div className="ball b2"></div>
+            <div className="ball b3"></div>
+            <div className="ball b4"></div>
+            <div className="ball b5"></div>
+            <div className="ball b6"></div>
+            <div className="ball b7"></div>
+            <div className="ball b8"></div>
+        </div>
+    )
+}
+
+const FORMULARIO = props => {
+    return (
+        <form className="formulario" method="POST" onSubmit={props.function}>
+            <fieldset>
+                <legend>Inserir comentário</legend>
+                <input type="text" name="nome" placeholder="Digite seu nome"/>
+                <input type="email" name="email" placeholder="Digite seu email"/>
+                <textarea name="message" rows="4"/>
+                <button type="submit">Adicionar</button>
+            </fieldset>
+        </form>
+    )
+}
 
 export default Comentario;
+export {BUTTON, LOADING, FORMULARIO};
